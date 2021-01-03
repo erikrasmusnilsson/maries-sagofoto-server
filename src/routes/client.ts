99adfafb04aeac50639bfcb4f2a34a1f8990b509
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2021-01-03 15:29:40 +0100

    Fixed client index.html path

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const router = express.Router();
 
 router.get('/*', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, 'public', 'client', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../public', 'client', 'index.html'));
 });
 
 export { router as clientRouter };
