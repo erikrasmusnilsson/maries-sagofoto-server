5128e636aa472295a394a82fbec24f8c548752c6
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2021-02-06 14:48:06 +0100

    Edited https redirection

--- src/middlewares/https-redirect.ts
+++ src/middlewares/https-redirect.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
 const httpsRedirect = (req: Request, res: Response, next: NextFunction) => {
-    if (req.get("Upgrade-Insecure-Requests") === "1") {
+    if (req.get("Upgrade-Insecure-Requests")) {
+        console.log("Upgrading request...")
         res.redirect(`https://${req.headers.host}${req.url}`);
     } else {
         next();
