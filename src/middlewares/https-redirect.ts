9b9ddeabb9e51cf2aecdd3383d5e608dd0db1eba
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2021-02-06 14:50:04 +0100

    Edited https redirection

--- src/middlewares/https-redirect.ts
+++ src/middlewares/https-redirect.ts
@@ -5,6 +5,7 @@ const httpsRedirect = (req: Request, res: Response, next: NextFunction) => {
         console.log("Upgrading request...")
         res.redirect(`https://${req.headers.host}${req.url}`);
     } else {
+        console.log("Using HTTP without encryption...")
         next();
     }
 }
